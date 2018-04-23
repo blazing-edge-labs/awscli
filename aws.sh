ab28cb23d5cc17aa6ef065d489866daa8ae7fc86
Stipe Kotarac <stipe@kotarac.net>
2018-04-23 16:15:20 +0200

    move the image to blazingedge org

--- aws.sh
+++ aws.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -e AWS_ACCESS_KEY_ID=$1 -e AWS_SECRET_ACCESS_KEY=$2 -e AWS_DEFAULT_REGION=$3 ${@:4} kotarac/awscli:latest
+docker run -it --rm -e AWS_ACCESS_KEY_ID=$1 -e AWS_SECRET_ACCESS_KEY=$2 -e AWS_DEFAULT_REGION=$3 ${@:4} blazingedge/awscli:latest
